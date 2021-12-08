04cb7a1afdcb5bf3941e1c0fb3ad32872f00abde
iTzP4iN <fdbautista90@gmail.com>
2021-12-08 05:31:39 -0300

    AOP - AfterReturning

---
+++ spring-demo-aop-z-after-returning/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,69 @@
+package com.luv2code.aopdemo.aspect;
+
+import java.util.List;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import com.luv2code.aopdemo.Account;
+
+@Aspect
+@Component
+@Order(2)
+public class MyDemoLoggingAspect {
+	
+	//add a new advice for @AfterReturning on the the findsAccounts method
+	
+	@AfterReturning(
+			pointcut="execution(* com.luv2code.aopdemo.dao.AccountDAO.findAccounts(..))",
+			returning="result")
+	public void afterReturningFindaccountsAdvice(
+			         JoinPoint theJoinPoint, List<Account> result){
+		
+		//print out which method we are advising on
+		String method = theJoinPoint.getSignature().toShortString();
+		System.out.println("\n=======>>> Executing @AfterReturning on method: " + method);
+		
+		//print out the results of the method call
+		System.out.println("\n=======>>> Result is: " + result);
+		
+	}
+	
+	//this is where we add all of our related advices for logging
+	
+	//let's start with an @Before advice
+	
+	@Before("com.luv2code.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterStter()")
+    public void beforeAddAccountAdvice(JoinPoint theJoinPoint) {
+		System.out.println("\n=====>>> Executing @Before advice on method");
+		
+		//display the method signature
+		MethodSignature methodSig = (MethodSignature) theJoinPoint.getSignature();
+		
+		System.out.println("Method: " + methodSig);
+				
+		//display method arguments
+		
+		//get args
+		Object[] args = theJoinPoint.getArgs();
+		
+		//loop thru args
+		for (Object tempArg : args) {
+			System.out.println(tempArg);
+			
+			if (tempArg instanceof Account) {
+				//downcast and print Account specific stuff
+				Account theAccount = (Account) tempArg;
+				
+				System.out.println("account name: " + theAccount.getName());
+				System.out.println("account level: " + theAccount.getLevel());				
+			}
+		}
+	}
+}
